c6c27c796786fd4a8e9633e73cb1ac796fdad605
Patrik Sandahl <patrik.sandahl@gmail.com>
2017-10-16 14:53:14 +0200

    Skeleton stuff.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # gl-scene-math
-A set of math utility functions adapted for gl-scene applications.
---
+++ src/Scene/Math.hs
@@ -0,0 +1,10 @@
+-- |
+-- Module: Scene.Math
+-- Copyright: (c) 2017 Patrik Sandahl
+-- Licence: MIT
+-- Maintainer: Patrik Sandahl <patrik.sandahl@gmail.com>
+-- Stability: experimental
+-- Portability: portable
+module Scene.Math
+    (
+    ) where
